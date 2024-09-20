a45ddac6b029f6d93e7b073953772663e3fe20af
Aime1177 <aimeflavien7@gmail.com>
2024-09-20 13:04:36 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import {ref,onMounted,computed,watch } from 'vue'
+
+const todos = ref([])
+const name = ref('')
+
+const inputContent = ref('')
+const inputCategories = ref(null)
+
+</script>
+
+<template>
+  <header>
+   <h1 class="font-bold text-3xl">Hello World</h1>
+  </header>
+
+  
+</template>
+
